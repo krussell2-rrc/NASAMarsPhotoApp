1eeb03235e07a581c9ec701373936b62d1cb7869
krussell2-rrc <143433171+krussell2-rrc@users.noreply.github.com>
2025-09-04 12:42:37 -0500

    Added functionality to select a rover and prompted the user with a date to view pictures taken by said Rover taken on said date

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 from requests import get
 import json
+from dateutil.parser import parse
 from PIL import Image
 from io import BytesIO
 from menu import Menu
+
+def selected_rover(rover):
+  roverMenu.close()
+  date = parse(input(f"\nEnter the date you wanted to see pictures from taken by the {rover} Rover : ")).strftime("%Y-%m-%d")
+
+roverMenu = Menu(
+  title="Mars Rover Photo App",
+  message="Choose a Rover:",
+  prompt="> ",
+  options=[
+    ("Curiosity", lambda: selected_rover("Curiosity")),
+    ("Opportunity", lambda: selected_rover("Opportunity")),
+    ("Spirit", lambda: selected_rover("Spirit")),
+    ("Perseverance", lambda: selected_rover("Perseverance"))
+  ]
+)
+
+roverMenu.open()
