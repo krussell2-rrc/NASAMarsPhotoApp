2d4ebebfddd81537b7fd867b838e4ebbc4fe86cc
krussell2-rrc <143433171+krussell2-rrc@users.noreply.github.com>
2025-09-04 18:48:29 -0500

    Added functionality to fetch photos from the NASA API based on the rover selected and date provided

--- main.py
+++ main.py
@@ -5,19 +5,29 @@ from PIL import Image
 from io import BytesIO
 from menu import Menu
 
-def selected_rover(rover):
+API_KEY ='xKBYhH8wTdCpfdIup0tfpDraeGtOdNRwuKJ2W9J9'
+photo_urls = []
+
+def fetch_photos(rover):
   roverMenu.close()
   date = parse(input(f"\nEnter the date you wanted to see pictures from taken by the {rover} Rover : ")).strftime("%Y-%m-%d")
+
+  url = f"https://api.nasa.gov/mars-photos/api/v1/rovers/{rover}/photos?earth_date={date}&api_key={API_KEY}"
+  photos = get(url).json()
+
+  for photo_url in photos['photos']:
+    photo_urls.append(photo_url['img_src'])
+
 
 roverMenu = Menu(
   title="Mars Rover Photo App",
   message="Choose a Rover:",
   prompt="> ",
   options=[
-    ("Curiosity", lambda: selected_rover("Curiosity")),
-    ("Opportunity", lambda: selected_rover("Opportunity")),
-    ("Spirit", lambda: selected_rover("Spirit")),
-    ("Perseverance", lambda: selected_rover("Perseverance"))
+    ("Curiosity", lambda: fetch_photos("Curiosity")),
+    ("Opportunity", lambda: fetch_photos("Opportunity")),
+    ("Spirit", lambda: fetch_photos("Spirit")),
+    ("Perseverance", lambda: fetch_photos("Perseverance"))
   ]
 )
 
