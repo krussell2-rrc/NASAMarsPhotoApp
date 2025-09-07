b92a36f2686612790133863cbc9316ed22ae211a
krussell2-rrc <143433171+krussell2-rrc@users.noreply.github.com>
2025-09-06 19:38:02 -0500

    Added the photos Menu that will provide the user with a list of pictures to select from and view

--- main.py
+++ main.py
@@ -7,9 +7,22 @@ from menu import Menu
 
 API_KEY ='xKBYhH8wTdCpfdIup0tfpDraeGtOdNRwuKJ2W9J9'
 photo_urls = []
+image_options = []
+
+def display_photo(url):
+  img_resp = get(url)
+  img = Image.open(BytesIO(img_resp.content))
+  img.show()
+  img.close()
+
+photosMenu = Menu(
+  title="Mars Rover Photo App",
+  message="Choose a photo to view:",
+  prompt="> ",
+  options=image_options
+)
 
 def fetch_photos(rover):
-  roverMenu.close()
   date = parse(input(f"\nEnter the date you wanted to see pictures from taken by the {rover} Rover : ")).strftime("%Y-%m-%d")
 
   url = f"https://api.nasa.gov/mars-photos/api/v1/rovers/{rover}/photos?earth_date={date}&api_key={API_KEY}"
@@ -18,6 +31,12 @@ def fetch_photos(rover):
   for photo_url in photos['photos']:
     photo_urls.append(photo_url['img_src'])
 
+  image_options = [
+    (url, lambda: display_photo(url)) for url in photo_urls
+    ] + [("Back", Menu.CLOSE)]
+
+  photosMenu.set_options(image_options)
+  photosMenu.open()
 
 roverMenu = Menu(
   title="Mars Rover Photo App",
